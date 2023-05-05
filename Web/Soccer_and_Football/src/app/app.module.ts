58d504d5feb5aa8d7efe83c5ff2a8160a6d5c717
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-05-05 11:46:18 +0200

    Adding project to branch develop web

---
+++ Web/Soccer_and_Football/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialImportsModule } from './modules/material-imports.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialImportsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Web/Soccer_and_Football/src/app/interface/user.interface.ts
@@ -0,0 +1,9 @@
+export interface UserResponse {
+    id:           string;
+    username:     string;
+    avatar:       string;
+    first_name:   string;
+    last_name:    string;
+    token:        string;
+    refreshToken: string;
+}
